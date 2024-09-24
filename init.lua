0a9ce851450f508387b96b929f57173cb6b2913c
Mike Conroy <mikecon@outlook.com>
2024-09-24 20:42:45 +0100

    Add File path to titlebar

--- init.lua
+++ init.lua
@@ -160,6 +160,11 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Set the file name in the titlebar.
+vim.opt.title = true
+vim.opt.titlelen = 0
+vim.opt.titlestring = 'nvim ' .. vim.fn.expand '%:p'
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
